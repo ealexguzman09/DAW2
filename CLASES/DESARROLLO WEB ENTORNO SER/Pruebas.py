4096c52973007acdf5fe6c92a6ced23dcedbc592
alu01 <ealexguzman47@gmail.com>
2024-09-09 14:00:23 +0200

    09/09/24

--- CLASES/DESARROLLO WEB ENTORNO SER/Pruebas.py
+++ CLASES/DESARROLLO WEB ENTORNO SER/Pruebas.py
@@ -1,1 +1,10 @@
 print("Hello, World!")
+
+import random
+print(random.randrange(1,10))
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
