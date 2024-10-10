d2a45f4d0122e08041a3846deb86f987e028c666
alu01 <ealexguzman47@gmail.com>
2024-10-10 13:57:07 +0200

    10/10/24

--- CLASES/DESARROLLO WEB ENTORNO SER/Ejercicios/EjecicioPython.py
+++ CLASES/DESARROLLO WEB ENTORNO SER/Ejercicios/EjecicioPython.py
@@ -7,8 +7,8 @@ engine = create_engine('postgresql://miusuario:erick@localhost/mydb')
 #Numero 2
 Base = declarative_base()
 from sqlalchemy import Column, Integer, String
-class miTabla(Base):
-    __tablename__ = 'mitabla'
+class TablaNueva(Base):
+    __tablename__ = 'Nuevaaaaa'
     id = Column(Integer, primary_key=True)
     nombre = Column(String)
 Base.metadata.create_all(engine)
@@ -16,7 +16,7 @@ Base.metadata.create_all(engine)
 #Numero 3
 Sesion = sessionmaker(bind=engine)
 sesion = Sesion()
-nuevo_registro = miTabla(nombre="Alex")
+nuevo_registro = TablaNueva(nombre="Alex")
 sesion.add(nuevo_registro)
 sesion.commit()
 sesion.close()
