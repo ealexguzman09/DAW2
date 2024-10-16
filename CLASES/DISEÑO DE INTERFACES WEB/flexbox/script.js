c3adf68cb1d74480f9da260b2cda1c9189016b3e
alu01 <ealexguzman47@gmail.com>
2024-10-16 13:06:56 +0200

    16/10/24

--- CLASES/DISEÑO DE INTERFACES WEB/flexbox/script.js
+++ CLASES/DISEÑO DE INTERFACES WEB/flexbox/script.js
@@ -1,5 +1,5 @@
 async function fetchDatosJSON() {
-    const url = 'https://api.open-meteo.com/v1/forecast?latitude=43.3128&longitude=-1.975&current=temperature_2m,is_day,weather_code'; // Replace with the desired URL
+    const url = 'https://api.open-meteo.com/v1/forecast?latitude=43.3128&longitude=-1.975&current=temperature_2m,weather_code';
     try {
         const respuesta = await fetch(url);
 
@@ -7,7 +7,8 @@ async function fetchDatosJSON() {
             const datosJSON = await respuesta.json();
 
             // Mostrar datos JSON
-            document.getElementById('contenidoJSON').textContent = JSON.stringify(datosJSON, null, 2);
+            document.getElementById('JSON').innerHTML = datosJSON.current.temperature_2m;
+            console.log(datosJSON.current.temperature_2m);
         } else {
             console.error('Fallo al procesar el archivo JSON:', respuesta.statusText);
         }
@@ -15,3 +16,4 @@ async function fetchDatosJSON() {
         console.error('Error en la conexión o en el mostrado de datos JSON:', error);
     }
 }
+fetchDatosJSON();
